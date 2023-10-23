a7cd8d09054739464206f4a555000a94162f3560
appjedi <appjedi.net@gmail.com>
2023-10-22 20:00:40 -0700

    update mydao.ts
    update mydao.ts

--- server/dao/MyDAO.ts
+++ server/dao/MyDAO.ts
@@ -10,7 +10,7 @@ interface DBConnection {
   dialect: string;
 }
 let connection: DBConnection;
-let User;
+let User: any;
 /*
 const User = sequelize.define("users", {
   username: {
@@ -21,7 +21,6 @@ const User = sequelize.define("users", {
     type: DataTypes.STRING,
     allowNull: false
   },
-
   roleId: {
     type: DataTypes.INTEGER,
   }
@@ -82,6 +81,6 @@ class MyDAO {
   };
   getConn = () => {
     return sequelize;
-  }
+  };
 }
 export default MyDAO;
