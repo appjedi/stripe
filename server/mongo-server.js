a0c4c736bad85a38e45e2c1ce45c23224d081b92
Bob T <54815862+appjedi@users.noreply.github.com>
2024-01-03 07:23:11 -0800

    update repo
    update repo

--- server/mongo-server.js
+++ server/mongo-server.js
@@ -2,34 +2,32 @@ const { MongoClient } = require("mongodb");
 const connectionString = "mongodb://localhost:27017/";
 const client = new MongoClient(connectionString);
 let conn;
-const connect= async()=>{
+const connect = async () => {
     try {
         conn = await client.connect();
-    } catch(e) {
-    console.error(e);
+    } catch (e) {
+        console.error(e);
     }
 }
 
 
-async function post()
-{
+async function post() {
     await connect();
     let db = conn.db("sample_training");
     let collection = await db.collection("posts");
-  let newDocument ={id:1, message:"Hello World"};
-  newDocument.date = new Date();
-  let result = await collection.insertOne(newDocument);
+    let newDocument = { id: 1, message: "Hello World" };
+    newDocument.date = new Date();
+    let result = await collection.insertOne(newDocument);
     console.log(result);
 }
-async function get()
-{
+async function get() {
     await connect();
     let db = conn.db("sample_training");
     let collection = await db.collection("posts");
     let results = await collection.find({})
-      .limit(50)
-      .toArray();
-    console.log(results);    
+        .limit(50)
+        .toArray();
+    console.log(results);
 }
 //post();
 get();
